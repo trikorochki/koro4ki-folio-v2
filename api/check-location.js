6afb964c26cf3d45e6f6d6cfc9b9ec15178085b6
evgeny <evgeny.ganshin@gmail.com>
2025-07-22 16:04:14 +0300

    Add location detection API endpoint

--- api/check-location.js
+++ api/check-location.js
@@ -4,6 +4,7 @@ export default function handler(req, res) {
   
   const isRussian = country_code === 'RU';
   
+  // CORS заголовки
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
